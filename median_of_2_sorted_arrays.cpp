0a5b99cfee8b5587c20e0dd26b0d5a0f28444144
P4RTH <parthparmar4507@gmail.com>
2021-12-02 18:06:55 +0530

    median bug solved

--- median_of_2_sorted_arrays.cpp
+++ median_of_2_sorted_arrays.cpp
@@ -40,7 +40,7 @@ public:
             p2++;
             p3++;
         }
-        if (n3 & 1 == 1)
+        if ((n3 & 1) == 1)
         {
             return tmp[(n3 - 1) / 2];
         }
@@ -53,19 +53,12 @@ class solution2
 public:
     float median(int a1[], int a2[], int n1, int n2)
     {
-        //write your code here
         int p1 = 0, p2 = 0, p3 = 0, n3 = n1 + n2;
         int previous = min(a1[p1], a2[p2]);
         int current = previous;
         while (p1 < n1 && p2 < n2)
         {
-            if (a1[p1] < a2[p2])
-            {
-                previous = current;
-                current = a1[p1];
-                p1++;
-            }
-            else if (a1[p1] > a2[p2])
+            if (a1[p1] > a2[p2])
             {
                 previous = current;
                 current = a2[p2];
@@ -77,17 +70,16 @@ public:
                 current = a1[p1];
                 p1++;
             }
-            if (n3 & 1 == 0 && p3 == (n3 / 2))
+            if ((n3 & 1) == 0 && p3 == (n3 / 2))
             {
                 // the median will be n3/2 + n3/2+1/2
-                return (previous + current) / 2;
+                return (previous + current) / 2.0;
             }
-            else if (n3 & 1 == 1 && p3 == n3 / 2 + 1)
+            else if ((n3 & 1) == 1 && p3 == (n3 / 2))
             {
                 //the median will be at n3/2+1 th position
                 return current;
             }
-
             p3++;
         }
         while (p1 < n1)
@@ -95,12 +87,12 @@ public:
             previous = current;
             current = a1[p1];
             p1++;
-            if (n3 & 1 == 0 && p3 == (n3 / 2))
+            if ((n3 & 1) == 0 && p3 == (n3 / 2))
             {
                 // the median will be n3/2 + n3/2+1/2
-                return (previous + current) / 2;
+                return (previous + current) / 2.0;
             }
-            else if (n3 & 1 == 1 && p3 == n3 / 2 + 1)
+            else if ((n3 & 1) == 1 && p3 == (n3 / 2))
             {
                 //the median will be at n3/2+1 th position
                 return current;
@@ -113,29 +105,28 @@ public:
             previous = current;
             current = a2[p2];
             p2++;
-            if (n3 & 1 == 0 && p3 == (n3 / 2))
+            if ((n3 & 1) == 0 && p3 == (n3 / 2))
             {
                 // the median will be n3/2 + n3/2+1/2
-                return (previous + current) / 2;
+                return (previous + current) / 2.0;
             }
-            else if (n3 & 1 == 1 && p3 == n3 / 2 + 1)
+            else if ((n3 & 1) == 1 && p3 == (n3 / 2))
             {
                 //the median will be at n3/2+1 th position
                 return current;
             }
             p3++;
         }
-        if (n3 & 1 == 0 && p3 == (n3 / 2))
+        if ((n3 & 1) == 0 && p3 == (n3 / 2))
         {
             // the median will be n3/2 + n3/2+1/2
-            return (previous + current) / 2;
+            return (previous + current) / 2.0;
         }
-        else if (n3 & 1 == 1 && p3 == n3 / 2 + 1)
+        else if ((n3 & 1) == 1 && p3 == (n3 / 2))
         {
             //the median will be at n3/2+1 th position
             return current;
         }
-
         return 0;
     }
 };
