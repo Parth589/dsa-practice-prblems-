f49481ec0d36f8689c82b28834f287402162e9db
P4RTH <parthparmar4507@gmail.com>
2021-12-25 13:02:10 +0530

    median of sorted arrays

--- Searching/median_of_2_sorted_array.cpp
+++ Searching/median_of_2_sorted_array.cpp
@@ -17,15 +17,25 @@ public:
         {
             int i1 = (low + high) / 2;
             int i2 = ((n1 + n2 + 1) / 2) - i1;
+            int max1 = (i1 == 0) ? INT_MIN : a[i1 - 1];
+            int max2 = (i2 == 0) ? INT_MIN : b[i2 - 1];
+            int min1 = (i1 == n1 - 1) ? INT_MAX : a[i1];
+            int min2 = (i2 == n2 - 1) ? INT_MAX : b[i2];
             //check if reached the mid
-            if (max(a[i1 - 1], b[i2 - 1]) < min(a[i1], b[i2]))
+            if (max(max1, max2) < min(min1, min2))
             {
-                return (max(a[i1 - 1], b[i2 - 1]) + min(a[i1], b[i2])) / 2.0f;
+                if (n1 + n2 & 1 == 1)
+                {
+                    return max(max1, max2);
+                }
+                return (max(max1, max2) + min(min1, min2)) / 2.0f;
             }
+            //if first array have smaller element ,go to right
             else if (a[i1] < max(a[i1 - 1], b[i2 - 1]))
             {
                 low = i1 + 1;
             }
+            // if second array have smaller element go to left
             else
             {
                 high = i1 - 1;
@@ -39,10 +49,10 @@ solution obj;
 int main()
 {
     //Driver code
-    int n1 = 4, n2 = 5;
+    int n1 = 4, n2 = 4;
     // cin >> n1 >> n2;
     int arr[n1] = {1, 5, 8, 12};
-    int arr2[n2] = {4, 7, 11, 56, 60};
+    int arr2[n2] = {4, 7, 11, 56};
     cout << obj.median(arr, arr2, n1, n2) << endl;
     return 0;
 }
